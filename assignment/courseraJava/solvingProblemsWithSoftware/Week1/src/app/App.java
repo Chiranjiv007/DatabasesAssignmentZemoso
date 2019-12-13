e1af9ca4d72c7244303529340a8337bfd70ed6fd
Chiranjiv007 <chiranjiv@iitg.ac.in>
2019-12-13 11:52:47 +0530

    Revert "Initial commite Java section coursera and problems"
    This reverts commit bee16bdbe6c13188d047e7c977de92ed19217f95.

--- assignment/courseraJava/solvingProblemsWithSoftware/Week1/src/app/App.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-import java.lang.Exception;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-    }
-}
--- assignment/courseraJava/src/chrianjiv/coursera_java/Main.java
+++
@@ -1,9 +0,0 @@
-package chrianjiv.coursera_java;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-	// write your code here
-    }
-}
